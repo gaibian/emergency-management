b478daf3c751668211d7eb06ee85c7ee080244ee
zhouquan <1327195241@qq.com>
2019-06-19 11:50:40 +0800

    zhouquan2019-6-19 am

--- src/api/index.js
+++ src/api/index.js
@@ -6,21 +6,17 @@ import login from './module/login'
 import aidIndex from './module/aidIndex'
 // 车辆信息模块
 import carAdmin from './module/carAdmin'
-<<<<<<< HEAD
+
 // 人员信息模块
 import personInfo from './module/personInfo'
-=======
+
 // 中心信息模块
 import centerAdmin from './module/centerAdmin'
->>>>>>> 668ba64d9dc07927053abb7ca86b0073b7f44160
 export {
   login,
   aidIndex,
   carAdmin,
-<<<<<<< HEAD
-  personInfo
-=======
+  personInfo,
   centerAdmin
->>>>>>> 668ba64d9dc07927053abb7ca86b0073b7f44160
 }
 
